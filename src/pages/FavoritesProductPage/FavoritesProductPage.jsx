9f6de86a3e418cd542488fc558987c7803c1a0ee
sokate <wolk4444@icloud.com>
2023-06-05 14:20:39 +0300

    Добавлена страница при пустом списке избранных товаров

--- src/pages/FavoritesProductPage/FavoritesProductPage.jsx
+++ src/pages/FavoritesProductPage/FavoritesProductPage.jsx
@@ -3,6 +3,7 @@ import { CardList } from "../../components/CardList/CardList";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { CardsContext } from "../../context/Context";
+import { ReactComponent as IconSmile} from "./Icons/smile.svg"
 
 
 export const FavoritesProductPage = () => {
@@ -10,10 +11,19 @@ export const FavoritesProductPage = () => {
     const {favoritesCards} = useContext(CardsContext)
 
     
-    return <div className="favorite__page__container">
+    return !!favoritesCards.length ?
+    <div className="favorite__page__container">
         <Link className="favorite__btn__link" to="/my_dogfood">
             <button className="favorite__btn__back" type="button">Назад</button></Link>
         <h1>Избранное</h1> 
         <CardList cards={favoritesCards}/>
     </div>
+    :
+    <div className="favorites__null__items__container">
+        <IconSmile/>
+        <h1>В Избранном пока ничего нет</h1>
+        <p>Добавляйте товары в Избранное с помощью ❤️️</p>
+        <Link to="/my_dogfood"><button type="button" >На главную</button></Link>
+    </div>
+
 }
