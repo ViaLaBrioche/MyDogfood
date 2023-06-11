731b9ffc534f549508ebf0f0115a2d3480ffe91c
sokate <wolk4444@icloud.com>
2023-06-11 14:50:01 +0300

    Изменение названия папки

--- src/components/ResetPasswordForm/ResetPasswordForm.jsx
+++ src/components/ResetPasswordForm/ResetPasswordForm.jsx
@@ -5,10 +5,10 @@ import { ModalContext } from "../../context/Context";
 
 export const ResetPasswordForm = () => {
 
-    const {resetDataSubmit} = useContext(ModalContext)
-    const { register, handleSubmit } = useForm({})                                      
+        const {resetDataSubmit} = useContext(ModalContext)
+        const { register, handleSubmit } = useForm({})                                      
 
-    return  <form className="modal__form" onSubmit={handleSubmit(resetDataSubmit)}>
+        return  <form className="modal__form" onSubmit={handleSubmit(resetDataSubmit)}>
                 <h1>Восстановление пароля</h1> 
                 <span>Для получения временного пароля необходимо ввести email, указанный при регистрации.</span>
                 <input name="email" placeholder="Почта" type="text" {...register("email", { required: "Пожалуйста, укажите почту" })} />
