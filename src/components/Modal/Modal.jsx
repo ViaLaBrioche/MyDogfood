7c044edd1f617794fa28cbf51401d97ad36fe00f
sokate <wolk4444@icloud.com>
2023-06-18 20:09:14 +0300

    Блокировка заднего фона при открытом модальном окне

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -8,8 +8,10 @@ export const Modal = ()=> {
 
     const {closeModal, modalIsOpen, isForm} = useContext(ModalContext)
 
-    return <div className={`modal__container ${!!modalIsOpen ? "modal__active" : null}`}>
-            <div className="modal__btn__close" onClick={()=> closeModal()}><Close className="modal__close__btn"/></div>
-            {isForm}
-    </div>
+    return  <div className={`overlay ${!!modalIsOpen ? "overlay__active" : null}`}>
+                <div className={`modal__container ${!!modalIsOpen ? "modal__active" : null}`}>
+                    <div className="modal__btn__close" onClick={()=> closeModal()}><Close className="modal__close__btn"/></div>
+                    {isForm}
+                </div>
+            </div>
 }
