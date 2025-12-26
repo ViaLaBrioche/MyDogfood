cf5a41a7bb610f08dd750dd4cbc2a6e73c7c2044
ekaterinavolkova@MacBook-Air <120165646+ViaLaBrioche@users.noreply.github.com>
2025-12-26 13:11:28 +0300

    Update Api.jsx
    token for getProductById

--- src/components/Api/Api.jsx
+++ src/components/Api/Api.jsx
@@ -23,7 +23,7 @@ export class Api {
     getProductById = (id) => {
         return fetch(`${this.baseUrl}products/${id}`, { 
             headers: {
-            authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDU4NzFhMmUwYmYyYzUxOWI5Y2NmYmUiLCJncm91cCI6IjEyIiwiaWF0IjoxNjgzNTE3ODcwLCJleHAiOjE3MTUwNTM4NzB9.US7rv52pRMThoo6sbhQeetW87zCYwxUuqZ6yZO2iS3w`,
+            authorization: `Bearer ${JSON.parse(localStorage.getItem('token'))}`,
             }})
             .then(res => {
             console.log(res)
