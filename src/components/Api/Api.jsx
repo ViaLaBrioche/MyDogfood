894b975ba3d2834b5194c49a608b93202f968052
ekaterinavolkova@MacBook-Air <120165646+ViaLaBrioche@users.noreply.github.com>
2025-12-26 13:12:58 +0300

    Update Api.jsx
    token

--- src/components/Api/Api.jsx
+++ src/components/Api/Api.jsx
@@ -177,7 +177,7 @@ export class Api {
     getAllReviewsById = (id) => {
         return fetch(`${this.baseUrl}products/review/${id}`, {
                 headers: {
-                    authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDU4NzFhMmUwYmYyYzUxOWI5Y2NmYmUiLCJncm91cCI6IjEyIiwiaWF0IjoxNjgzNTE3ODcwLCJleHAiOjE3MTUwNTM4NzB9.US7rv52pRMThoo6sbhQeetW87zCYwxUuqZ6yZO2iS3w`,
+                    authorization: `Bearer ${JSON.parse(localStorage.getItem('token'))}`,
                     'Content-Type': 'application/json'
                 },
             }).then(res => {
